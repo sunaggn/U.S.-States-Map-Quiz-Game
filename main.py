04cce3d49d7c1ea75bc56b88d4fc2546026a527d
sunaggn <sunagegin@gmail.com>
2025-02-07 23:24:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title= "Guess the State", prompt="Name another")
+print(answer_state)
+
+
+
+
+
+
+
+screen.exitonclick()
